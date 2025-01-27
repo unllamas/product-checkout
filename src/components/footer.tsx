5230fc801e248538c220c194dea6ce6c09887dc9
Jona <llamasjonat@gmail.com>
2025-01-27 05:00:24 -0300

    fix(component): change name on Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className='w-full bg-gray-100 body-font'>
       <div className='flex justify-center p-4'>
         <p className='text-muted-foreground text-sm text-center'>
-          © 2025 Product Checkout —{' '}
+          © 2025 Llamout —{' '}
           <Link className='border-b pb-1' rel='noopener noreferrer' target='_blank' href='/asd'>
             @unllamas
           </Link>
