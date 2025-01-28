a13e83df270a2410bce80c03df6f7761a6e08ad0
Jona <llamasjonat@gmail.com>
2025-01-28 00:22:54 -0300

    fix: general changes

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,4 +1,5 @@
 export const COMPANY = {
+  // show image 48x48
   image: 'https://pbs.twimg.com/profile_images/1869194441426083841/qxwTWYtj_400x400.jpg',
   name: 'Jona |🇦🇷',
   website: 'https://x.com/unllamas',
@@ -8,15 +9,19 @@ export const COMPANY = {
 export const CHECKOUT = {
   success_url: 'https://www.jonallamas.com/',
   cancel_url: '',
+  // '' or 'donate'
   submit_type: 'donate',
+  // only 'en'
   locale: 'en',
 };
 
 export const PRODUCT = {
+  // 4/3 aspect ratio
   image:
     'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3luN2UzMDZtcWdsaXhyeGk4ZmU1NTR2MXF5aGJxOTV4M2g1OW4xbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FdpN7wrCGqlrR24eHw/giphy.gif',
   name: 'My eternal love',
   description: 'Acquire the love of a Llama using the system!',
   price: 1001,
+  // only 'SAT'
   currency: 'SAT',
 };
