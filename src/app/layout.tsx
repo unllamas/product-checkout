88fe2a19abf3449fb6504c1438772c7928180482
Jona <llamasjonat@gmail.com>
2025-01-29 06:04:31 -0300

    fix(app): cleaned layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 import './globals.css';
-import { CHECKOUT } from '@/mock';
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
-
-export const metadata: Metadata = {
-  title: 'Llamout - Payment System',
-  description: 'By @unllamas',
-};
 
 export default function RootLayout({
   children,
@@ -21,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang={CHECKOUT?.locale}>
+    <html lang='en'>
       <body className={`${inter.variable} antialiased`}>
         <div className='relative overflow-x-hidden flex flex-col w-screen min-h-screen bg-background'>{children}</div>
         <Analytics />
